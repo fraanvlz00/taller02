fd823011db624ec2ee7faecf68f7ec5cbc2d5f38
Fransheska <fransheskavalenzuela@gmail.com>
2024-04-18 11:01:27 -0400

    commit 3

--- src/main/java/org/example/Parte3menu.java
+++ src/main/java/org/example/Parte3menu.java
@@ -4,27 +4,23 @@ import java.util.Scanner;
 
 public class Parte3menu {
     double[][] sismos;
+
     Parte3menu(){
         this.sismos = sismos;
-    }
 
-
-    public static void scanner(){
-        Scanner sc = new Scanner(System.in);
     }
 
     public static void dimensionArreglo(double[][] sismos, int dias){
         sismos = new double[dias][24];
     }
 
-    public static double recorreArray (double[][] sismos, double[][] sismos) {
+    public static void recorreArray (double[][] sismos) {
+        Scanner sc = new Scanner(System.in);
         for (int i = 0; i < sismos.length; i++) {
             for (int j = 0; j < sismos[i].length; j++) {
-                sismos[i][j] = scanner().nextDouble();
-    }}}
-    public static void datosHora(double datoHora){
-
-    }
+                System.out.println("Ingresa el dato del dia "+i+" a la hora "+j+":00 hrs");
+                sismos[i][j] = sc.nextDouble();
+    }}}}
 
 
 
