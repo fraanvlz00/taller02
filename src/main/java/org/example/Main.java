e09984a63b95f8fe0747f9cfbd3ec2d0b64414ef
Ignacio <ivaldebenito52@gmail.com>
2024-04-18 10:04:58 -0400

    3er commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,25 +13,18 @@ public class Main {
         return sismos;
     }
 
-    public static void rellenoSismos(double sismos[][]) {
-        for (int i = 0; i < sismos.length; i++) {
-            for (int j = 0; j < sismos[i].length; j++) {
-                sismos[i][j] = 1 + (Math.random() * 8.5);
+
+public static int[] contarSismosGrado4(double[][] sismos) {
+    int[] contadorPorDia = new int[sismos.length];
+    for (int i = 0; i < sismos.length; i++) {
+        for (int j = 0; j < sismos[i].length; j++) {
+            if (sismos[i][j] >= 4) {
+                contadorPorDia[i]++;
             }
         }
     }
-
-    public static void mostrarSismos(double sismos[][]) {
-        for (int i = 0; i < sismos.length; i++) {
-            System.out.println();
-            for (int j = 0; j < sismos[i].length; j++) {
-                System.out.print("|" + sismos[i][j] + "|");
-            }
-        }
-        System.out.println();
+    return contadorPorDia;
     }
-
-
     public static void menu (){
         double[][] sismos = crearMatriz(7, 24);
 
@@ -40,29 +33,25 @@ public class Main {
         do {
             System.out.println("Menu");
             System.out.println("1.- Dias A registrar");
-            System.out.println("2.- Mostrar sismo de mayor magnitud");
-            System.out.println("3.- Contar sismos mayores o iguales a 5.0");
-            System.out.println("4.- Enviar SMS por cada sismo mayor o igual a 7.0");
-            System.out.println("5.- Mostar Tablilla de sismos Semanales");
-            System.out.println("6.- Salir");
+            System.out.println("2.- Mostrar sismos ≥ 4.0 grados Richter");
+            System.out.println("3.- Reiniciar el arreglo con nuevos valores");
+            System.out.println("4.- Salir");
             try{
             opcion = teclado.nextInt();
             switch (opcion) {
                 case 1:
-                    rellenoSismos(sismos);
+
                     break;
                 case 2:
-
+                    int[] sismosGrado4OMayor = contarSismosGrado4(sismos);
+                    for (int i = 0; i < sismosGrado4OMayor.length; i++) {
+                        System.out.println("Día " + (i + 1) + ": " + sismosGrado4OMayor[i] + " veces");
+                    }
                     break;
                 case 3:
 
                     break;
                 case 4:
-                    break;
-                case 5:
-                    mostrarSismos(sismos);
-                    break;
-                case 6:
                     System.out.println("Bye Bye");
                     System.exit(0);
                 default:
