604464e765463321b08376233bb9518e0d6fab5e
Fransheska <fransheskavalenzuela@gmail.com>
2024-04-18 09:29:22 -0400

    commit 1

---
+++ src/main/java/org/example/menu3_.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class
+
+public class menu3_ {
+    double[][] sismos;
+    menu3_(sismos){
+        this.sismos = sismos;
+
+    }
+
+    public static void dimensionArreglo(double[][] sismos, int dias){
+        sismos = new double[dias][24];
+    }
+    public static void ingresoDatosArreglo(double[][] ,double datoUsuario){
+
+
+        }
+    }
+    public static double[][] recorreArray (double[][] sismos){
+        for (int i = 0; i < sismos.length; i++){
+            return = sismos[i];
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
