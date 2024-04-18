4a88ca91d92677898eac1575fe8ffb7b3c8cd1d6
Fransheska <fransheskavalenzuela@gmail.com>
2024-04-18 10:56:18 -0400

    commit 2

---
+++ src/main/java/org/example/Parte3menu.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Parte3menu {
+    double[][] sismos;
+    Parte3menu(){
+        this.sismos = sismos;
+    }
+
+
+    public static void scanner(){
+        Scanner sc = new Scanner(System.in);
+    }
+
+    public static void dimensionArreglo(double[][] sismos, int dias){
+        sismos = new double[dias][24];
+    }
+
+    public static double recorreArray (double[][] sismos, double[][] sismos) {
+        for (int i = 0; i < sismos.length; i++) {
+            for (int j = 0; j < sismos[i].length; j++) {
+                sismos[i][j] = scanner().nextDouble();
+    }}}
+    public static void datosHora(double datoHora){
+
+    }
+
+
+
+
+
+
+}
+
