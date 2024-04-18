4301fe5aa52c5c21013002ccaf567fc516dc2cdc
Ignacio <ivaldebenito52@gmail.com>
2024-04-18 11:48:30 -0400

    4rto commit

---
+++ src/main/java/org/example/SismosMasIntenso.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Random;
+
+class SismoMasIntenso {
+    public double[][] lecturas;
+
+    public SismoMasIntenso(int dias) {
+        lecturas = new double[dias][24];
+        generarLecturasAleatorias();
+    }
+
+    private void generarLecturasAleatorias() {
+        Random random = new Random();
+
+        for (int i = 0; i < lecturas.length; i++) {
+            for (int j = 0; j < lecturas[i].length; j++) {
+                lecturas[i][j] = 1.0 + (9.5 - 1.0) * random.nextDouble();
+            }
+        }
+    }
+
+    public void MostrarSismoMasIntenso() {
+        double max = Double.MIN_VALUE;
+        int dia = 0;
+        int hora = 0;
+
+        for (int i = 0; i < lecturas.length; i++) {
+            for (int j = 0; j < lecturas[i].length; j++) {
+                if (lecturas[i][j] > max) {
+                    max = lecturas[i][j];
+                    dia = i + 1;
+                    hora = j;
+                }
+            }
+        }
+        System.out.println("El día " + dia + ", a las " + hora + ":00, se registró el sismo más intenso, con un valor de " + max + " en la escala de Richter.");
+    }
+}
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,21 +2,28 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
-    public void testContarSismosGrado4() {
+    void mostrarSismosGrado4() {
         double[][] sismos = {
-                {3.5, 4.2, 3.9, 4.5},
-                {4.1, 3.8, 4.3, 4.7},
-                {3.6, 4.0, 4.2, 3.7}
+                {3.5, 4.2, 3.9, 4.5, 2.8},
+                {4.1, 3.7, 4.3, 3.8, 4.6},
+                {3.6, 4.0, 4.2, 3.5, 4.1}
         };
-        int[] expected = {2, 3, 2};
-        assertArrayEquals(expected, Main.contarSismosGrado4(sismos));
-        int[] expected2 = {3, 3, 3};
-        assertArrayEquals(expected2, Main.contarSismosGrado4(sismos));
-    }
 
+        String expectedOutput = "Día 1: 3 veces\nDía 2: 4 veces\nDía 3: 3 veces\n";
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        Main.mostrarSismosGrado4(sismos);
+
+        assertEquals(expectedOutput, outputStream.toString());
+    }
 }
+
